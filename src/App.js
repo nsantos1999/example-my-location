9da53f728cfaa77d125bc281874d7cd6d3218942
Nata Santos <natasouza.santos50@gmail.com>
2020-05-22 19:49:34 -0300

    Example location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ location, setLocation ] = useState({});
+
+	useEffect(() => {
+		navigator.geolocation.watchPosition(onLocationReceived);
+	},[]);
+
+	const onLocationReceived = location => {
+		const { latitude, longitude } = location.coords;
+		setLocation({
+			latitude,
+			longitude
+		})
+		console.log(location);
+	} 
+
+	return (
+		<div>
+			Latitude: {location.latitude} <br />
+			Longitude: {location.longitude}
+		</div>
+	);
 }
 
 export default App;
